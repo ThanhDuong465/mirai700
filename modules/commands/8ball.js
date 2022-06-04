2ca5a7ee8d5bd07525b6247ddd5e58a43d7b63d1
ThanhDuong465 <102492688+ThanhDuong465@users.noreply.github.com>
2022-06-04 22:44:29 +0700

    Hjv

--- modules/commands/8ball.js
+++ modules/commands/8ball.js
@@ -1,24 +1,837 @@
-module.exports.config = {
-	name: "8ball",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "VanHung",
-	description: "8ball",
-	commandCategory: "Hình ảnh",
-	usages: "boy",
-	cooldowns: 5
-};
-module.exports.run = async ({ api, event }) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-	axios.get('https://nekos.life/api/v2/img/8ball').then(res => {
-	let anh = res.data.url.substring(res.data.url.lastIndexOf(".") + 1);
-	let callback = function () {
-					api.sendMessage({
-						attachment: fs.createReadStream(__dirname + `/cache/tl.${anh}`)
-					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/tl.${anh}`), event.messageID);
-				};
-				request(res.data.url).pipe(fs.createWriteStream(__dirname + `/cache/tl.${anh}`)).on("close", callback);
-			})
-}
+[
+  {
+    "data":"Chớ nghe lời phỉnh tiếng phờ\nThò tay vào lờ, mắc kẹt cái hom"
+  },
+  {
+    "data":"Măng chua nấu cá ngạch nguồn\nSự đời đắp đổi, khi buồn khi vui."
+  },
+  {
+    "data":"Yêu nhau xé lụa may quần\nGhét nhau kể nợ kể nần nhau ra"
+  },
+  {
+    "data":"Ai ơi chớ vội cười nhau\nCây nào mà chẳng có sâu chạm cành"
+  },
+  {
+    "data":"Khi giàu chẳng có đỡ ai\nĐến khi hoạn nạn chẳng ai đỡ mình"
+  },
+  {
+    "data":"Ở sao cho vừa lòng người\nỞ rộng người cười, ở hẹp người chê."
+  },
+  {
+    "data":"Chim khôn chưa bắt đã bay,\nNgười khôn ít nói, ít hay trả lời"
+  },
+  {
+    "data":"Thuốc đắng dã tật, sự thật mất lòng."
+  },
+  {
+    "data":"Một lời nói dối, sám hối bảy ngày."
+  },
+  {
+    "data":"Nói ngọt lọt đến xương"
+  },
+  {
+    "data":"Miếng ngon nhớ lâu, lời đau nhớ đời"
+  },
+  {
+    "data":"Lưỡi sắc hơn gươm"
+  },
+  {
+    "data":"Lưỡi không xương, nhiều đường lắt léo."
+    },
+    {
+    "data":"Sẩy chân còn hơn sẩy miệng."
+    },
+    {
+    "data":"Tiếng lành đồn xa, tiếng dữ đồn xa."
+    },
+    {
+    "data":"Một người thì kín, hai người thì hở."
+    },
+    {
+    "data":"Nói thì dễ, làm thì khó."
+    },
+    {
+    "data":"Tre non dễ uốn"
+    },
+    {
+    "data":"Bé chẳng vin, cả gãy cành."
+    },
+    {
+    "data":"Yêu cho vọt, ghét cho chơi"
+    },
+    {
+    "data":"Mẹ dạy thì con khéo, bố dạy thì con khôn."
+    },
+    {
+    "data":"Học ăn học nói, học gói học mở"
+    },
+    {
+    "data":"Dốt đến đâu, học lâu cũng biết."
+    },
+    {
+    "data":"Người vụng đan thúng giữa đường."
+    },
+    {
+    "data":"Không thầy đố mày làm nên."
+    },
+    {
+    "data":"Học thày không tày học bạn."
+    },
+    {
+    "data":"Có tích mới dịch nên tuồng."
+    },
+    {
+    "data":"Văn hay chẳng lo dài dòng."
+    },
+    {
+    "data":"Văn mình, vợ người."
+    },
+    {
+    "data":"Ở hiền gặp lành."
+    },
+    {
+    "data":"Ở tinh gặp ma, ở quỷ gặp quái, gian tà gặp nhau."
+    },
+    {
+    "data":"Nọc người bằng mười nọc rắn."
+    },
+    {
+    "data":"Một đời làm hại, bại hoại ba đời."
+    },
+    {
+    "data":"Hùm giết người hùm ngủ,\nNgười giết người thức đủ năm canh."
+    },
+    {
+    "data":"Phụ vợ, không gặp vợ."
+    },
+    {
+    "data":"Hiền quá hóa ngu."
+    },
+    {
+    "data":"Giết một con cò, cứu trăm con tép."
+    },
+    {
+    "data":"Đẹp nết hơn đẹp người."
+    },
+    {
+    "data":"Cái nết đánh chết cái đẹp."
+    },
+    {
+    "data":"Đói cho sạch, rách cho thơm."
+    },
+    {
+    "data":"Chết trong còn hơn sống đục."
+    },
+    {
+    "data":"Xấu hay làm tốt, dốt hay nói chữ."
+    },
+    {
+    "data":"Ai ưa dưa khú, bầu già."
+    },
+    {
+    "data":"Tốt đẹp phô ra, xấu xa đậy lại."
+    },
+    {
+    "data":"Bạc đầu chưa hết dại."
+    },
+    {
+    "data":"Khôn ba năm, dại một giờ."
+    },
+    {
+    "data":"Người khôn ăn nói nửa chừng\nĐể cho người dại vừa mừng vừa lo."
+    },
+    {
+    "data":"Khôn làm văn tế, dại làm văn bia."
+    },
+    {
+    "data":"Ăn cỗ đi trước, lội nước đi sau."
+    },
+    {
+    "data":"Chẳng có dại nào giống dại nào."
+    },
+    {
+    "data":"Ngu si hưởng thái bình"
+    },
+    {
+    "data":"Thằng dại làm hại thằng khôn."
+    },
+    "data":"Khôn cho người ta sợ,\nDại cho người ta thương,\nDở dở ương ương tổ cho người ta ghét."
+    },
+    "data":"Vụng chèo khéo chống."
+    },
+    "data":"Vụng múa chê đất lệch."
+    },
+    "data":"Hay thì khen, hèn thì chê."
+    },
+    "data":"Mười quan tiền công,\n    Không bằng một đồng tiền thưởng."
+    },
+    "data":"Trâu chết để da, người chết để tiếng."
+    },
+    "data":"Tốt danh hơn lành áo."
+    },
+    "data":"Lắm người yêu hơn nhiều kẻ ghét."
+    },
+    "data":"Yêu trẻ, trẻ đến nhà; yêu già, già để phúc."
+    "data":[
+  {
+    "data":"Chớ nghe lời phỉnh tiếng phờ\nThò tay vào lờ, mắc kẹt cái hom"
+  },
+  {
+    "data":"Măng chua nấu cá ngạch nguồn\nSự đời đắp đổi, khi buồn khi vui."
+  },
+  {
+    "data":"Yêu nhau xé lụa may quần\nGhét nhau kể nợ kể nần nhau ra"
+  },
+  {
+    "data":"Ai ơi chớ vội cười nhau\nCây nào mà chẳng có sâu chạm cành"
+  },
+  {
+    "data":"Khi giàu chẳng có đỡ ai\nĐến khi hoạn nạn chẳng ai đỡ mình"
+  },
+  {
+    "data":"Ở sao cho vừa lòng người\nỞ rộng người cười, ở hẹp người chê."
+  },
+  {
+    "data":"Chim khôn chưa bắt đã bay,\nNgười khôn ít nói, ít hay trả lời"
+  },
+  {
+    "data":"Thuốc đắng dã tật, sự thật mất lòng."
+  },
+  {
+    "data":"Một lời nói dối, sám hối bảy ngày."
+  },
+  {
+    "data":"Nói ngọt lọt đến xương"
+  },
+  {
+    "data":"Miếng ngon nhớ lâu, lời đau nhớ đời"
+  },
+  {
+    "data":"Lưỡi sắc hơn gươm"
+  },
+  {
+    "data":"Lưỡi không xương, nhiều đường lắt léo."
+    },
+    {
+    "data":"Yêu nhau chín bỏ làm mười\nCó mặt thì mắng, vắng mặt thì thương."
+    },
+    {
+    "data":"Yêu nhau lắm, cắn nhau đau."
+    },
+    {
+    "data":"Yêu nên tốt, ghét nên xấu."
+    },
+    {
+    "data":"Yêu ai yêu cả đường đi,\nGhét nhau ghét cả tông chi họ hàng."
+    },
+    {
+    "data":"Ghét nhau đào đất đổ đi"
+    },
+    {
+    "data":"Người có lúc vinh lúc nhục,\nNước có lúc đục lúc trong."
+    },
+    {
+    "data":"Sướng lắm, khổ nhiều."
+    },
+    {
+    "data":"Thương người như thể thương thân"
+    },
+    {
+    "data":"Lá lành đùm lá rách\nĂn nhạt mới biết thương mèo."
+    },
+    {
+    "data":"Bỏ thì thương, vương thì tội."
+    },
+    {
+    "data":"Ăn quả nhớ kẻ trồng cây."
+    },
+    {
+    "data":"Uống nước nhớ nguồn."
+    },
+    {
+    "data":"Một đêm nằm một năm ở."
+    },
+    {
+    "data":"Đường mòn, ân nghĩa không mòn."
+    },
+    {
+    "data":"Chanh chua chớ phụ, ngọt bòng chớ ham."
+    },
+    {
+    "data":"Hoài thóc nuôi cò rừng"
+    },
+    {
+    "data":"Được chim bẻ ná, được cá quên nơm."
+    },
+    {
+    "data":"Ăn cháo đái bát.",
+    },
+    {
+    "data":"Chưa khỏi rên đã quên thầy.",
+    },
+    {
+    "data":"Có xương sông, tình phụ lá lốt.",
+    },
+    {
+    "data":"Có trăng, phụ đèn.",
+    },
+    }
+    "data":"Một câu nhịn là chín câu lành.",
+    },
+    {
+    "data":"Tránh voi chẳng xấu mặt.",
+    },
+    {
+    "data":"Con gà tức nhau tiếng gáy.",
+    },
+    {
+    "data":"Đánh nhau chia gạo, chào nhau ăn cơm.",
+    },
+    {
+    "data":"Cả giận mất khôn.",
+    },
+    {
+    "data":"Hơi đâu mà giận người dưng.",
+    },
+    {
+    "data":"Một đời kiện, chín đời thù.",
+    },
+    }
+    "data":"Ăn ngay nói thật, mọi tật mọi lành.",
+    },
+    {
+    "data":"Cây ngay chẳng sợ chết đứng",
+    },
+    {
+    "data":"Có tật giật mình, có tình kinh
+     trong bụng.",
+     },
+     {
+    "data":"Thẳng mực tàu đau lòng gỗ.",
+    },
+    {
+    "data":"Mất lòng trước, được lòng sau.",
+    },
+    {
+    "data":"Mật ngọt chết ruồi,\nnhững nơi cay đắng là nơi thật thà.",
+    },
+    {
+    "data":"Trâu đồng nào ăn cỏ đồng ấy.",
+    },
+    {
+    "data":"Ăn cây nào rào cây ấy.",
+    },
+    {
+    "data":"Thân trâu trâu lo, thân bò bò liệu.",
+    },
+    {
+    "data":"Cha chung không ai khóc.",
+    },
+    {
+    "data":"Cháy nhà hàng xóm, bình chân như vại.",
+    },
+    {
+    "data":"Hoài tiền mua pháo, mượn người đốt.",
+    },
+    {
+    "data":"Việc nhà thì nhác, việc chú bác thì siêng.",
+    },
+    {
+    "data":"Có vay có trả, mới thỏa lòng nhau."
+    },
+    {
+    "data":"Cá mè một lứa.",
+    },
+    {
+    "data":"Tắt đèn nhà ngói cũng như nhà tranh.",
+    },
+    {
+    "data":"Làm quan ăn lộc vua, ở chùa ăn lộc phật.",
+    },
+    {
+    "data":"Ngày dưng thì chẳng chắp gai,\nđến khi có cá mượn chài ai cho.",
+    },
+    {
+    "data":"Người lười đất không lười.",
+    },
+    {
+    "data":"Mồm miệng đỡ chân tay.",
+    },
+    {
+    "data":"Giã gạo thì ốm, giã cốm thì khỏe.",
+    },
+    {
+    "data":"Khéo ăn thì no, khéo co thì ấm.",
+    },
+    {
+    "data":"Đo bò làm chuồng.",
+    },
+    {
+    "data":"Yếu chân chạy trước.",
+    },
+    {
+    "data":"Làm khi lành, để dành khi đau",
+    },
+    {
+    "data":"Mất bò mới lo làm chuồng.",
+    },
+    {
+    "data":"Có mình thì giữ.",
+    },
+    {
+    "data":"Buôn tàu bán đất, không bằng ăn dè hà tiện.",
+    },
+    {
+    "data":"Đãi cứt sáo lấy hạt đa, đãi cứt gà lấy hạt tấm.",
+    },
+    {
+    "data":"Kiếm củi ba năm thiêu một giờ",
+    },
+    {
+    "data":"Đời cha vo tròn, đời con bóp bẹp"
+   },
+   {
+    "data":"Sông lở cát bồi",
+    },
+    {
+    "data":"Được lòng ta, xót xa lòng người",
+    },
+    {
+    "data":"Bói rẻ còn hơn ngồi không",
+    },
+    {
+    "data":"Chẳng được con trắm con chép, cũng được mớ tép mớ tôm",
+    },
+    {
+    "data":"Thả con săn sắt, bắt con cá rô",
+  },
+  {
+    "data":"Lọt sàng xuống nia",
+   },
+   {
+    "data":"Lá rụng về cội",
+    },
+    {
+    "data":"Cơm không ăn, gạo còn đó",
+    },
+    {
+    "data":"Gạo đổ, hót chẳng đầy thưng",
+    },
+    {
+    "data":"Mất cả chì lẫn chài",
+    },
+    {
+    "data":"Được một bữa cơm người, mất mười bữa cơm nhà.",
+    },
+    {
+    "data":"Đã khó, chó cắn thêm",
+    },
+    {
+    "data":"Biết tội đâu mà tránh, biết phúc đâu mà tìm.",
+    },
+    {
+    "data":"Buồn ngủ lại gặp chiếu manh\nhay ăn làm biếng gặp anh đứng đường.",
+    },
+    {
+    "data":"Đi đêm lắm có ngày gặp ma"
+    },
+    {
+    "data":"Bốn chín chưa qua, năm ba đã tới",
+    },
+    {
+    "data":"Tránh vỏ dưa, gặp vỏ dừa",
+    },
+    {
+    "data":"Chạy trời không khỏi nắng",
+    },
+    {
+    "data":"Thuốc chữa bệnh, chẳng chữa được mệnh",
+    },
+    {
+    "data":"Khi nên, trời cũng chiều người",
+    },
+    {
+    "data":"Năm ngón tay có ngón dài ngón ngắn",
+   },
+   {
+    "data":"Ai uốn câu cho vừa miệng cá",
+    },
+    {
+    "data":"Thánh cũng có khi nhầm",
+    },
+    {
+    "data":"Bới bèo ra bọ",
+    },
+    {
+    "data":"Đánh rắn phải đánh dập đầu",
+    },
+    {
+    "data":"Đã tu thời tu cho trót.",
+    },
+    {
+    "data":"Có chí thì nên.",
+    },
+    {
+    "data":"Mưu cao chẳng bằng chí dày",
+    },
+    {
+    "data":"Kiến tha lâu cũng đầy tổ"
+    },
+   {
+    "data":"Có công mài sắt, có ngày nên kim"
+    },
+    {
+    "data":"Năng nhặt chặt bị"
+    },
+    {
+    "data":"Trăm bó đuốc cũng vớ được con ếch"
+    },
+    {
+    "data":"Còn nước còn tát"
+    },
+    {
+    "data":"Thắng không kiêu, bại không nản"
+    },
+    {
+    "data":"Sóng cả chớ ngã tay chèo"
+    },
+    {
+    "data":"Vua thua thằng liều"
+    },
+    {
+    "data":"Con mẹ đẻ con con\nCòn gà trống gà mái thì còn gà giò "
+    },
+    {
+    "data":"Trời không đóng cửa ai"
+    },
+    {
+    "data":"Ai giàu ba họ, ai khó ba đời"
+    },
+    {
+    "data":"Cờ đến tay ai người ấy phất"
+    },
+    {
+    "data":"Bút sa, gà chết"
+    },
+    {
+    "data":"Xay lúa thì thôi ẵm em"
+    },
+    {
+    "data":"Lắm sãi không ai đóng cửa chùa"
+    },
+    {
+    "data":"Bụng làm da chịu"
+   },
+   {
+    "data":"Có gan ăn cắp, có gan chịu đòn"
+    },
+    {
+    "data":"Chó gầy hổ mặt người nuôi"
+    },
+    {
+    "data":"Bắn súng không nên, phải đền đạn"
+    },
+    {
+    "data":"Trăm dâu đổ đầu tằm "
+    },
+    {
+    "data":"Trọc đầu càng mát"
+    },
+    {
+    "data":"Mèo nhỏ bắt chuột con"
+    },
+    {
+    "data":"Rộng làm kép, hẹp làm đơn"
+    },
+    {
+    "data":"Ốc chẳng mang nổi mình ốc\nốc lại còn mang cọc cho rêu"
+    },
+    {
+    "data":"Ai dám đánh đu với tinh "
+    },
+    {
+    "data":"Chó ghẻ có mỡ đằng đuôi"
+    },
+    {
+    "data":"Chửa học bò đã lo học chạy"
+    },
+    {
+    "data":"Chưa võ bọng cứt đã đòi bay bổng"
+    },
+    {
+    "data":"Ếch ngồi đáy giếng, coi trời bằng vung"
+    },
+    {
+    "data":"Chó cậy gần nhà, gà cậy gần chuồng"
+    },
+    {
+    "data":"Thân lừa ưa nặng"
+    },
+    {
+    "data":"Mạnh dùng sức, yếu dùng chước "
+    },
+    {
+    "data":"Có cháo hỏi chè"
+    },
+    {
+    "data":"Đứng núi này trông núi nọ"
+    },
+    {
+    "data":"Điếc hay ngóng, ngọng hay nói"
+    },
+    {
+    "data":"Ăn mày đòi xôi gấc"
+    },
+    {
+    "data":"Già còn chơi trống bỏi"
+    },
+    {
+    "data":"Con có khóc mẹ mới cho bú"
+    },
+    {
+    "data":"Ai biết ngứa đâu mà gãi"
+   },
+   {
+    "data":"Ếch no khó nhử mồi"
+    },
+    {
+    "data":"Chê của nào trời trao của ấy"
+    },
+    {
+    "data":"Chê tôm lại phải ăn tôm,\nchê rau muống héo, lại ôm dưa già."
+    },
+    {
+    "data":"Ăn trứng thì đừng ăn con."
+    },
+    {
+    "data":"Được thể dễ nói khoác"
+    },
+    {
+    "data":"Tế sớm khỏi ruồi"
+    },
+    {
+    "data":"Gió chiều nào che chiều ấy"
+    },
+    {
+    "data":"Châu chấu thấy đỏ lửa thì vào."
+    },
+    {
+    "data":"Theo đóm ăn tàn"
+    },
+    {
+    "data":"Nhờ gió bẻ măng"
+    },
+    {
+    "data":"Đục nước béo cò"
+    },
+    {
+    "data":"Giậu đổ bìm leo"
+    },
+    {
+    "data":"Chợ chưa họp, kẻ cắp đã đến"
+    },
+    {
+    "data":"Vỏ quýt dày có móng tay nhọn"
+    },
+    {
+    "data":"Kẻ cắp gặp bà già"
+    },
+    {
+    "data":"Mềm nắn rắn buông"
+    },
+    {
+    "data":"Lành làm gáo, vỡ làm môi"
+    },
+    {
+    "data":"Hòn đất ném đi, hòn chì ném lại"
+},
+{
+    "data":"Mâm cao đánh ngã bát đầy"
+    },
+    {
+    "data":"Ở xứ mù anh chột làm vua"
+    },
+    {
+    "data":"Gần chùa gọi bụt bằng anh"
+    },
+    {
+    "data":"Cháy nhà ra mặt chuột"
+    },
+    {
+    "data":"Đâm lao phải theo lao"
+    },
+    {
+    "data":"Trót đa mang phải đeo bòng"
+    },
+    {
+    "data":"Sai một li đi một dặm"
+    },
+    {
+    "data":"Tham thì thâm, nhầm thì thiệt"
+    },
+    {
+    "data":"Chết sông chết suối,\nkhông ai chết đuối đọi đèn"
+    },
+    {
+    "data":"Có nước có cá"
+    },
+    {
+    "data":"Thóc đâu bồ câu đấy"
+    },
+    {
+    "data":"Nằm giữa không mất phần chân"
+    },
+    {
+    "data":"Cơm vào miệng còn rơi"
+    },
+    {
+    "data":"Một mất mười ngờ"
+    },
+    {
+    "data":"Cóc có gan cóc, kiến có gan kiến."
+    },
+    {
+    "data':"Máu trâu cũng như máu bò"
+    },
+    {
+    "data":"Lòng vả cũng như lòng sung"
+    },
+    {
+    "data":"Ba thưng cũng vào một đấu"
+    },
+    {
+    "data":"Suy bụng ta ra bụng người"
+    },
+    {
+    "data":"Bói ra ma, quét nhà ra rác"
+    },
+    {
+    "data":"Giang sơn đâu, anh hùng đấy"
+    },
+    {
+    "data":"Cơm đâu no chó, thóc đâu no gà"
+    },
+    {
+    "data":"Quá mù ra mưa."
+    },
+    {
+    "data":"Chín quá hóa nẫu"
+    },
+    {
+    "data":"Trèo cao ngã đau"
+    },
+    {
+    "data":"Thắm lắm phai nhiều"
+    },
+    {
+    "data":"Già néo đứt dây"
+    },
+    {
+    "data":"Chọn mặt gửi vàng"
+    },
+    {
+    "data":"Đi với phật mặc áo cà sa\nđi với ma mặc áo giấy"
+    },
+    {
+    "data":"To đầu khó chui"
+    },
+    {
+    "data":"Ở bầu thì tròn, ở ống thì dài"
+    },
+    {
+    "data":"Rau nào sâu nấy"
+    },
+    {
+    "data":"Gần lửa rát mặt"
+    },
+    {
+    "data":"Cháy thành vạ lây"
+    },
+    {
+    "data":"Gần nhà giàu đau răng ăn cốm\ngần kẻ trộm ốm lưng chịu đòn"
+    },
+    {
+    "data":"Lụt thì lụt cả làng"
+    },
+    {
+    "data":"Được mùa thầy chùa no bụng"
+    },
+    {
+    "data":"Trâu bò húc nhau, ruồi muỗi chết"
+    },
+    {
+    "data":"Rút dây động rừng"
+    },
+    {
+    "data":"Môi hở răng lạnh"
+    },
+    {
+    "data":"Đánh chó ngó chủ nhà"
+    },
+    {
+    "data":"Yêu con chị vị con em"
+    },
+    {
+  "data":"Cách sông nên phải lụy đò"
+  },
+  {
+    "data":"Há miệng mắc quai"
+    },
+    {
+    "data":"Muốn tròn thì phải có khuôn,\nmuốn vuông phải có thước"
+    },
+    {
+    "data":"Đầu xuôi đuôi lọt"
+    },
+    {
+    "data":"Vạn sự khởi đầu nan"
+    },
+    {
+    "data":"Cũ người mới ta"
+    },
+    {
+    "data":"Trước lạ sau quen"
+    },
+    {
+    "data":"Đánh bạc quen tay\nngủ ngày quen mắt"
+    },
+    {
+    "data":"Ăn vặt quen mồm"
+    },
+    {
+    "data":"Ngựa quen đường cũ"
+    },
+    {
+    "data":"Người làm sao, chiêm bao làm vậy"
+    },
+    {
+    "data":"Dốt đặc còn hơn hay chữ lỏng"
+    },
+    {
+    "data":"Tích tiểu thành đại"
+    },
+    {
+    "data":"Góp gió thành bão, góp cây nên rừng"
+    },
+    {
+    "data":"Ba người đánh một, không chột cũng què"
+    },
+    {
+    "data":"Vét nồi ba mươi cũng đầy niêu nốt"
+    },
+    {
+    "data":"Xấu đều hơn tốt lõi"
+    },
+    {
+    "data":"Xanh không thủng cá đi đằng nào"
+    },
+    {
+    "data":"Có bột mới gột nên hồ"
+    },
+    {
+    "data":"Ôm rơm nặng bụng"
+    },
+    {
+    "data":"Chơi dao sắc có ngày đứt tay"
+    },
+]
